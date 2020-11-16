0dadaa5b8faca3b935734e1db74f50c72f066500
Joel Andrew Thomas Greer <joel.greer@cern.ch>
2020-11-16 17:35:33 +0000

    added py3.7 to tox in this branch. flake8 and mypy commands succeeded but unknown problems with coverage of 0% for src/shine/analysis

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -14,3 +14,10 @@ def test_template_func_multiply_by_two(init_data):
     expected_data = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
     assert len(doubled_data) == len(init_data)
     np.testing.assert_almost_equal(doubled_data, expected_data, 5)
+
+
+def test_divide_by_two(init_data):
+    halved_data = analysis.divide_by_two(init_data)
+    expected_data = np.array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5])
+    assert len(halved_data) == len(init_data)
+    np.testing.assert_allclose(halved_data, expected_data)
